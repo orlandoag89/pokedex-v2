4a62bacd59a6ad0651704c4a53289fceb3440b4c
ANGELES GRANADOS, ORLANDO <oangeles@empresaexterna.com.mx>
2023-06-01 13:50:35 -0600

    maquetado de pokedex

--- src/app/shared/components/smarts/pkd2-pokedex/pkd2-pokedex.component.ts
+++ src/app/shared/components/smarts/pkd2-pokedex/pkd2-pokedex.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Pkd2PokedexComponent implements OnInit {
 
+  public showScreen: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
