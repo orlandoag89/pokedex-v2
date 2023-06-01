c670063b879247bbe646b69b87147be5197a61fe
ANGELES GRANADOS, ORLANDO <oangeles@empresaexterna.com.mx>
2023-06-01 12:09:26 -0600

    spinner

--- src/app/module/home/home.module.ts
+++ src/app/module/home/home.module.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { HomePageComponent } from "./home-page/home-page.component";
-import { CharacterModule } from "@smarts-components";
 import { PokeApiService } from "@core/services";
+import { CharacterModule } from "@smarts-components";
+import { SpinnerModule } from "@dumb-components";
+import { HomePageComponent } from "./home-page/home-page.component";
 
 @NgModule({
   declarations: [ HomePageComponent ],
   imports: [
     CommonModule,
-    CharacterModule
+    CharacterModule,
+    SpinnerModule
   ],
   providers: [
     PokeApiService
